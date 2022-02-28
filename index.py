127da110c7c7e153c497fad363984da03a827596
ahmed elzamarany <ahmed_elzamarany@hotmail.com>
2022-02-28 03:36:42 +0100

    fix

--- index.py
+++ index.py
@@ -1,14 +1,9 @@
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+
 # Connect to main app.py file
 from app import app
-from app import server
-import dash_bootstrap_components as dbc
 # import data
 
 # Connect to your app pages
-from apps import mainapp
 
 
 # app.layout = html.Div([
@@ -16,7 +11,7 @@ from apps import mainapp
     
 #     ,html.Div(id='page-content', children=[])
 # ])
-app.layout = mainapp.layout
+
 
 # @app.callback(Output('page-content', 'children'),
 #               [Input('url', 'pathname')])
@@ -27,5 +22,3 @@ app.layout = mainapp.layout
 #         return dbc.Button("Reviewers Statistics", href='/apps/Statistics', color="primary")
 
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
